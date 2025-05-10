1c48e7296cc7f116dee236c4b7c9ad6e873caf27
Štěpánka <Doma@Stepanka-MacBook-Air.local>
2025-05-10 13:55:11 +0200

    doplnění řešení Kontrola dostupnosti

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-const tlacitkoElement = document.querySelector('#button-order');
+// const tlacitkoElement = document.querySelector('#button-order');
 
-tlacitkoElement.addEventListener('click', () => {
-    tlacitkoElement.textContent = 'Objednáno';
-});
+// tlacitkoElement.addEventListener('click', () => {
+//     tlacitkoElement.textContent = 'Objednáno';
+// });
 
+
+// cvičení Kontrola dostupnosti
+const button = document.querySelector('#button-order');
+setTimeout(() => {
+    button.textContent = 'Objednat';
+    button.disabled = false;
+}, 8000);
+
+button.addEventListener('click', () => {
+    button.textContent = 'Objednáno';
+})
